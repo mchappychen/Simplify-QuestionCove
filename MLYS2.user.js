41dc73f92acd06e77f5b5bf3a4fd82117ce18c22
Michael <51720724+mchappychen@users.noreply.github.com>
2019-10-12 14:52:27 -0400

    Update MLYS2.user.js

--- MLYS2.user.js
+++ MLYS2.user.js
@@ -8,21 +8,33 @@
 // ==/UserScript==
 
 (function() {
-    
-    
-    window.deletedMessageIds = [];
-    if(localStorage.getItem("deletedMessageIds")){
-        window.deletedMessageIds = JSON.parse(localStorage.getItem("deletedMessageIds"));
-    } else {
-        localStorage.setItem("deletedMessageIds" , JSON.stringify(window.deletedMessageIds) );
+
+    function delay(){
+        window.deletedMessageIds = [];
+        if(localStorage.getItem("deletedMessageIds")){
+            window.deletedMessageIds = JSON.parse(localStorage.getItem("deletedMessageIds"));
+        } else {
+            localStorage.setItem('deletedMessageIds' , JSON.stringify(window.deletedMessageIds) );
+        }
+        let messages = document.getElementsByClassName('messages')[1].children;
+        if(messages.length){
+            for(let i=0;i<messages.length;i++){
+                //show delete button
+                messages[i].children[5].children[1].children[0].style.display = "block";
+                //add function to delete button to delete messages
+                let messageId = messages[i].getAttribute("data-id");
+                messages[i].children[5].children[1].children[0].setAttribute("onclick","window.deletedMessageIds.push('"+messageId+"');this.parentNode.parentNode.parentNode.style.display='none';localStorage.setItem('deletedMessageIds' , JSON.stringify(window.deletedMessageIds) );");
+                //If it's part of deletedMessageIds, hide it
+                if(window.deletedMessageIds.includes(messageId)){
+                    messages[i].style.display = "none";
+                }
+            }
+        } else {
+            setTimeout(delay,100);
+        }
     }
 
-    let messages = document.getElementsByClassName('messages')[1].children;
-    for(let i=0;i<messages.length;i++){
-        //show delete button
-        messages[i].children[5].children[1].children[0].style.display = "block";
-        //add function to delete button to delete messages
-     let messageId = messages[i].getAttribute("data-id");
-    messages[i].children[5].children[1].children[0].setAttribute("onclick","window.deletedMessageIds.push('"+messageId+"');this.parentNode.parentNode.parentNode.style.display='none'");
-    }
-})();
+    setTimeout(delay,100);
+
+
+})(document);
