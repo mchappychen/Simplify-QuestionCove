788913983b745681842dcab5453c16fe3bf8c407
Michael <51720724+mchappychen@users.noreply.github.com>
2020-03-18 17:54:40 -0400

    Update noNotif.user.js

--- noNotif.user.js
+++ noNotif.user.js
@@ -1,8 +1,15 @@
+window.unSubbed = [];
+
 questioncove.event = function(d, f) {
     var e = $.Event(d);
     if(e.type == "insert-notification"){
-       console.log("event",e.type);
        console.log("data",f);
+       if(!unSubbed.includes(f.notificationJson.id)){
+            for (g in f) {
+                e[g] = f[g]
+            }
+            return $(document).trigger(e)
+       }
     } else {
         for (g in f) {
             e[g] = f[g]
@@ -10,3 +17,22 @@ questioncove.event = function(d, f) {
         return $(document).trigger(e)
     }
 };
+
+function makeUnsubButton(){
+    if($(".update-list")[0]){
+        for(let i=0;i<$(".update-list")[0].children.length;i++){
+            try{
+                if(!$(".update-list")[0].children[i].children[2].innerHTML.includes("<button>")){
+                    let mhchenButton = document.createElement("button");
+                    mhchenButton.appendChild(document.createTextNode("Unsub"));
+                    mhchenButton.onclick = function(){unSubbed.push(this.parentNode.parentNode.getAttribute("data-id"))}
+                    $(".update-list")[0].children[i].children[2].appendChild(mhchenButton);
+                }
+            } catch(e){
+                //console.log(e);
+            }
+        }
+    }
+    setTimeout(makeUnsubButton,2000);
+}
+setTimeout(makeUnsubButton,4000);
